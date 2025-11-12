fe4957e44292bfeda248aa70e94a4144fcec278d
KangSuji <sjkang@yescnc.co.kr>
2025-11-12 12:18:21 +0900

    refactor: Hook으로 변경

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,22 +1,26 @@
-import React, { Component } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 import { MdDelete } from "react-icons/md";
 
-export default class ExpenseList extends Component {
-  render() {
-    return (
-      <>
-        <ul className="list">
-          {this.props.initialExpenses.map((expense) => {
-            return <ExpenseItem expense={expense} key={expense.id} />;
-          })}
-        </ul>
-        <button className="btn">
-          목록 지우기
-          <MdDelete className="btn-icon" />
-        </button>
-      </>
-    );
-  }
-}
+const ExpenseList = ({ initialExpenses, handleDelete }) => {
+  return (
+    <>
+      <ul className="list">
+        {initialExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              expense={expense}
+              key={expense.id}
+              handleDelete={handleDelete}
+            />
+          );
+        })}
+      </ul>
+      <button className="btn">
+        목록 지우기
+        <MdDelete className="btn-icon" />
+      </button>
+    </>
+  );
+};
+export default ExpenseList;
